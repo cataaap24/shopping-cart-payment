425c708ca27aca967c41b852e0971391f23e803f
c.pineda <c.pineda@unillanos.edu.co>
2026-05-20 18:50:37 -0500

    Add PaymentResult class for payment processing

--- src/main/java/isi/shoppingCart/usecases/dto/PaymentResult.java
+++ src/main/java/isi/shoppingCart/usecases/dto/PaymentResult.java
@@ -1,1 +1,27 @@
+package isi.shoppingCart.usecases.dto;
 
+public class PaymentResult {
+    private boolean approved;
+    private String message;
+
+    public PaymentResult(boolean approved, String message) {
+        this.approved = approved;
+        this.message = message;
+    }
+
+    public static PaymentResult approved(String message) {
+        return new PaymentResult(true, message);
+    }
+
+    public static PaymentResult rejected(String message) {
+        return new PaymentResult(false, message);
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
