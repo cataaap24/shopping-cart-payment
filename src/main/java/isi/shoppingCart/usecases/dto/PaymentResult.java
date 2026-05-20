73d7c321bc5ff047634d5bf3f6ac48b19b714a10
c.pineda <c.pineda@unillanos.edu.co>
2026-05-20 18:55:31 -0500

    Add reference field to PaymentResult class

--- src/main/java/isi/shoppingCart/usecases/dto/PaymentResult.java
+++ src/main/java/isi/shoppingCart/usecases/dto/PaymentResult.java
@@ -3,18 +3,20 @@ package isi.shoppingCart.usecases.dto;
 public class PaymentResult {
     private boolean approved;
     private String message;
+    private String reference;
 
-    public PaymentResult(boolean approved, String message) {
+    public PaymentResult(boolean approved, String reference, String message) {
         this.approved = approved;
         this.message = message;
+        this.reference = reference;
     }
 
-    public static PaymentResult approved(String message) {
-        return new PaymentResult(true, message);
+    public static PaymentResult approved(String reference, String message) {
+        return new PaymentResult(true, reference, message);
     }
 
-    public static PaymentResult rejected(String message) {
-        return new PaymentResult(false, message);
+    public static PaymentResult rejected(String reference, String message) {
+        return new PaymentResult(false, reference, message);
     }
 
     public boolean isApproved() {
@@ -24,4 +26,9 @@ public class PaymentResult {
     public String getMessage() {
         return message;
     }
+
+    public String getReference() {
+        return reference;
+    }
+
 }
