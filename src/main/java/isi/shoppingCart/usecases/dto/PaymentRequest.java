8e5e483d71166e09fb632c0e921e42206ef65e91
David Navarro <ju.da.navarro@unillanos.edu.co>
2026-05-21 22:04:41 -0500

    Microfuncionalidad asignada Implementar el servicio de pago simulado
    InMemoryFakePaymentGateway en infrastructure/repositories. PaymentRequest en usecases/dto. PaymentGateway.java en usecases/ports.

--- src/main/java/isi/shoppingCart/usecases/dto/PaymentRequest.java
+++ src/main/java/isi/shoppingCart/usecases/dto/PaymentRequest.java
@@ -1,4 +1,21 @@
 package isi.shoppingCart.usecases.dto;
 
 public class PaymentRequest {
+
+    private final String clientId;
+    private final double totalAmount;
+
+    public PaymentRequest(String clientId, double totalAmount) {
+        this.clientId = clientId;
+        this.totalAmount = totalAmount;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
 }
